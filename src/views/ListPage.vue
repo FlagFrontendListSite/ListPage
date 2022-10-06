10f51a9f62d6b2ab14164764dda7197799fa4a4b
KimJunpyo <bono1725@naver.com>
2022-10-06 21:35:57 +0900

    base2
    base2

--- src/views/ListPage.vue
+++ src/views/ListPage.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div>
     <table class="board">
       <thead>
@@ -24,39 +23,6 @@
       </tbody>
     </table>
   </div>
-=======
-  <body>
-    <header>
-      <img src="/src/assets/logo.JPG" width="300px" height="100px" />
-      <ul>
-        <button class="dropbtn"><h3>FLAG</h3></button>
-        <div class="dropdown-content">
-          <a href="#"></a>
-        </div>
-
-        <button class="dropbtn"><h3>BORDER</h3></button>
-        <div class="dropdown-content">
-          <a href="#"></a>
-        </div>
-        <button class="dropbtn"><h3>ACTIVITY</h3></button>
-        <div class="dropdown-content">
-          <a href="#">STUDY</a>
-          <a href="#">PROJECT</a>
-        </div>
-        <button class="dropbtn"><h3>NOTICE</h3></button>
-        <div class="dropdown-content">
-          <a href="#"></a>
-        </div>
-        <li>
-          <div class="search-bar">
-            <i class="fas fa-search" aria-hidden="true"></i>
-            <input class="search-bar__input" type="search" placeholder="검색" />
-          </div>
-        </li>
-      </ul>
-    </header>
-  </body>
->>>>>>> c014e9b968c1592ba3229734fdefe1929a963e65
 </template>
 <script>
 export default {
@@ -73,53 +39,7 @@ export default {
 </script>
 
 <style scoped>
-<<<<<<< HEAD
 .board {
   background-color: white;
 }
 </style>
-=======
-  body {
-  background-color: rgba(19, 16, 16, 0.973);
-  margin-top: 125px;
-  width: 100%;
-  height: 100%;
-  align-items: center;
-}
-a {
-  text-decoration-line: none;
-}
-header {
-  height: 65px;
-  padding-bottom: 40px;
-  margin-bottom: 20px;
-  background: white;
-  font-weight: bold;
-  display: flex;
-  justify-content: space-between;
-  flex-direction: row;
-  position: fixed;
-  top: 0;
-  /* width: 100% */
-  left: 0;
-  right: 0;
-}
-ul {
-  display: flex;
-}
-li {
-  margin: 30px;
-  list-style-type: none;
-}
-.container {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  border: 2px groove rgba(19, 16, 16, 0.973);
-}
-</style>
->>>>>>> c014e9b968c1592ba3229734fdefe1929a963e65
