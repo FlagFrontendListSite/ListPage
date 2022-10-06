fa7149f1910b9a9ed8bb73a098b7d9a27da34d90
KimJunpyo <bono1725@naver.com>
2022-10-06 21:33:01 +0900

    base
    base

--- src/views/ListPage.vue
+++ src/views/ListPage.vue
@@ -1,13 +1,34 @@
 <template>
-  <div></div>
+  <div>
+    <table class="board">
+      <thead>
+        <tr>
+          <th></th>
+          <th>제목</th>
+          <th>작성자</th>
+          <th>작성일</th>
+          <th>조회수</th>
+          <th>댓글</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 <script>
 export default {
   components: {},
   data() {
-    return {
-      sampleData: "",
-    };
+    return {};
   },
   setup() {},
   created() {},
@@ -16,3 +37,9 @@ export default {
   methods: {},
 };
 </script>
+
+<style scoped>
+.board {
+  background-color: white;
+}
+</style>
